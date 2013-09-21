5618ea75d67400e0dfc131f7b1278e633b75aa0e
Ilya Kantor <iliakan@gmail.com>
2013-09-21 15:17:17 +0400

    move indexes creation time to after dropDatabase

--- chat/9/createDb.js
+++ chat/9/createDb.js
@@ -1,6 +1,5 @@
 var mongoose = require('lib/mongoose');
 var async = require('async');
-var User = require('models/user').User;
 
 async.series([
   open,
@@ -21,27 +20,18 @@ function dropDatabase(callback) {
 }
 
 function createUsers(callback) {
-  async.parallel([
-    function(callback) {
-      var vasya = new User({username: 'Вася', password: 'supervasya'});
-      vasya.save(function(err) {
-        callback(err, vasya);
-      });
-    },
-    function(callback) {
-      var petya = new User({username: 'Петя', password: '123'});
-      petya.save(function(err) {
-        callback(err, petya);
-      });
-    },
-    function(callback) {
-      var admin = new User({username: 'admin', password: 'thetruehero'});
-      admin.save(function(err) {
-        callback(err, admin);
-      });
+  require('models/user');
 
-    }
-  ], callback);
+  var users = [
+    {username: 'Вася', password: 'supervasya'},
+    {username: 'Петя', password: '123'},
+    {username: 'admin', password: 'thetruehero'}
+  ];
+
+  async.each(users, function(userData, callback) {
+    var user = new mongoose.models.User(userData);
+    user.save(callback);
+  }, callback);
 }
 
 function close(callback) {
--- chat/9/routes/index.js
+++ chat/9/routes/index.js
@@ -6,6 +6,7 @@ module.exports = function(app) {
 
   app.get('/login', require('./login').get);
   app.post('/login', require('./login').post);
+
   app.post('/logout', require('./logout').post);
 
   app.get('/chat', checkAuth, require('./chat').get);
